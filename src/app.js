e06e32711f64df6359e98d9855c190e92fdb47bd
Yuvaraj <yuviy2006@gmail.com>
2025-01-08 00:10:19 +0530

    Added refresh token functionality and updated related code

--- src/app.js
+++ src/app.js
@@ -8,14 +8,13 @@ app.use(cors({
 }
 ))
 
-app.use(express.json({limit:"16kb"}));
-app.use(express.urlencoded({extended:true},{limit:"16kb"}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));  // Corrected this line
 app.use(express.static("public"));
 app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.routes.js";
-
 //routes declaration
 app.use("/api/v1/users",userRouter)
 
